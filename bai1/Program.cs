5797a7494dab7a2e2525f4c2a7b2a35e1757eb3f
Hale <86954218+Halenopro@users.noreply.github.com>
2024-11-08 20:28:47 +0700

    BTVN-Week3

---
+++ bai1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace week2;
+
+class Program
+{
+    static void Main(string[] args)
+    {   
+        Console.WriteLine("Ve hinh chu nhat: ");
+        Console.WriteLine("Nhap chieu rong: ");
+        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap chieu dai: ");
+        int y = int.Parse(Console.ReadLine());
+
+        for (int t = 0; t < x; t++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                if (t == 0 || t == x - 1 || j == 0 || j == y - 1)
+                {
+                    Console.Write("*");
+                }
+
+                else
+                {
+                    Console.Write(" ");
+                    ;
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        int a, b;
+        Console.WriteLine("Ve hinh tam giac can: ");
+        Console.WriteLine("Nhap do dai canh ben: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int k = 1; k <= n-1; k++)
+        {
+            Console.Write(" ");//In dong dau
+        }
+        Console.Write("*\n");
+        int i = 2;
+        while (i <= n - 1)
+        {
+            a = n - i;
+            for (int k=1 ; k <= a; k++)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("*");
+
+            b = 2 * i - 3;
+            for (int k = 1; k <= b; k++)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("*\n");
+            i++;
+
+        }
+
+        for (int k = 1; k <= 2 * n - 1; k++)
+        {
+            Console.Write("*");
+        }
+    }
+}   
---
+++ bai3/ConsoleApp3/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bai3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> Manage = new Dictionary<string, Dictionary<string, int>>();
+
+            
+            Add(Manage);
+            Add(Manage);
+
+            FindTopEmployee(Manage);
+            FindTopProduct(Manage);
+        }
+
+        
+        static void Add(Dictionary<string, Dictionary<string, int>> Manage)
+        {
+            Console.Write("Add employee's name: ");
+            string employeeName = Console.ReadLine();
+
+            Console.Write("Product name: ");
+            string productName = Console.ReadLine();
+
+            Console.Write("Quantity: ");
+            int quantity = int.Parse(Console.ReadLine());
+
+            if (!Manage.ContainsKey(employeeName))
+            {
+                Manage[employeeName] = new Dictionary<string, int>();
+            }
+
+            if (Manage[employeeName].ContainsKey(productName))
+            {
+                Manage[employeeName][productName] += quantity;
+            }
+            else
+            {
+                Manage[employeeName][productName] = quantity;
+            }
+        }
+
+        
+        static void FindTopEmployee(Dictionary<string, Dictionary<string, int>> Manage)
+        {
+            string topEmployee = null;
+            int maxTotalQuantity = 0;
+
+            foreach (var employee in Manage)
+            {
+                int employeeTotal = 0;
+                foreach (var product in employee.Value)
+                {
+                    employeeTotal += product.Value;
+                }
+
+                if (employeeTotal > maxTotalQuantity)
+                {
+                    maxTotalQuantity = employeeTotal;
+                    topEmployee = employee.Key;
+                }
+            }
+
+            if (topEmployee != null)
+            {
+                Console.WriteLine($"The Saler sales most: {topEmployee} sum Quantity {maxTotalQuantity}");
+            }
+            else
+            {
+                Console.WriteLine("No data.");
+            }
+        }
+
+        
+        static void FindTopProduct(Dictionary<string, Dictionary<string, int>> Manage)
+        {
+            Dictionary<string, int> productTotals = new Dictionary<string, int>();
+
+            foreach (var employee in Manage)
+            {
+                foreach (var product in employee.Value)
+                {
+                    if (productTotals.ContainsKey(product.Key))
+                    {
+                        productTotals[product.Key] += product.Value;
+                    }
+                    else
+                    {
+                        productTotals[product.Key] = product.Value;
+                    }
+                }
+            }
+
+            string topProduct = null;
+            int maxProductQuantity = 0;
+
+            foreach (var product in productTotals)
+            {
+                if (product.Value > maxProductQuantity)
+                {
+                    maxProductQuantity = product.Value;
+                    topProduct = product.Key;
+                }
+            }
+
+            if (topProduct != null)
+            {
+                Console.WriteLine($"Best product: {topProduct} sum Quantity {maxProductQuantity}");
+            }
+            else
+            {
+                Console.WriteLine("No data.");
+            }
+        }
+    }
+}
